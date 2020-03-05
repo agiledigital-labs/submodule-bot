c110e797615eb2ccb4b0187576d392eaae25352b
Haolin Jiang <hljiang@agiledigital.com.au>
2020-03-06 10:02:28 +1100

    feat: init

---
+++ index.js
@@ -0,0 +1,78 @@
+const fs = require('fs-extra');
+
+const workingDirPath = './repos';
+
+// if (fs.existsSync(workingDirPath)) {
+//   console.log(`Cleaning working directory [${workingDirPath}]`);
+//   fs.removeSync(workingDirPath);
+// }
+
+if (!fs.existsSync(workingDirPath)){
+  console.log(`Creating temp working directory [${workingDirPath}]`);
+  fs.mkdirSync(workingDirPath, { recursive: true });
+}
+
+const simpleGit = require('simple-git/promise');
+const git = simpleGit(workingDirPath);
+const axios = require('axios');
+
+const bitbucketUsername = process.env.BITBUCKET_USERNAME;
+const bitbucketPassword = process.env.BITBUCKET_PASSWORD;
+
+const run = async () => {
+  const response = await axios.get('https://stash.agiledigital.com.au/rest/api/latest/projects/repos?limit=1000', {
+    auth: {
+      username: bitbucketUsername,
+      password: bitbucketPassword
+    }
+  });
+
+  const repos = response.data.values;
+
+  repos.map(async repo => {
+    const repoDir = `${workingDirPath}/${repo.name}`;
+
+    const httpCloneUrl = repo.links.clone.find(link => link.name === 'http').href;
+
+    const host = httpCloneUrl.split('://')[1];
+  
+    const cloneUrlWithCred = `https://${bitbucketUsername}:${encodeURIComponent(bitbucketPassword)}@${host}`;
+  
+    const repoDirExist = fs.existsSync(repoDir);
+
+    if (!repoDirExist){
+      console.log(`Cloning [${repo.name}]...`);
+      await git.clone(cloneUrlWithCred);
+      console.log(`Done [${repo.name}]`);
+    }
+
+    await git.cwd(repoDir);
+
+    await git.subModule([
+      'update',
+      '--init',
+      '--recursive'
+    ]);
+
+    const submodules = await git.raw([
+      'config',
+      '--file',
+      '.gitmodules',
+      '--name-only',
+      '--get-regexp',
+      'path'
+    ]);
+
+    
+    console.log(submodules);
+
+    return 'Done';
+  });
+
+
+};
+
+run();
+
+
+
